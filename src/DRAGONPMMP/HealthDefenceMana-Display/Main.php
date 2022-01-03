4633def6d26de1a95eb8665e8ed0aabc587b78c4
DRAGONPMMP <93898371+DRAGONPMMP@users.noreply.github.com>
2022-01-03 12:13:08 +0530

    Create Main.php

---
+++ src/DRAGONPMMP/HealthDefenceMana-Display/Main.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace DRAGONPMMP\HealthDefenceMana-Display;
+
+use pocketmine\Player;
+
+use pocketmine\Server;
+
+use pocketmine\plugin\Plugin;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\event\Listener;
+
+use pocketmine\event\player\PlayerJoinEvent;
+
+class HealthDefenceMana-Display extends PluginBase implements Listener {
+
+         public function onEnable(){
+
+                  $this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+                  @mkdir($this->getDataFolder());
+
+                  $this->saveDefaultConfig();
+
+                  $this->getResources("config.yml");
+
+         }
+
+         public function onJoin(PlayerJoinEvent $event){
+
+                  $player = $event->getPlayer();
+
+                  $this->getScheduler()->scheduleRepeatingTask(new \pocketmine\scheduler\ClosureTask(function(int $unused) : void{
+
+                           foreach($this->getServer()->getOnlinePlayers() as $player){
+
+                                    $health = $player->getHealth();
+
+                                    $maxhealth = $player->getMaxHealth();
+
+                                    $defence = $player->getArmorPoints();
+
+                                    $player->sendActionbarMessage("§c{$health}/{$maxhealth}heart  §a{$defence}❈Defense  §b100.0✎Mana");              
+
+                           }
+
+                  }), 20);
+
+         }
+
+}
